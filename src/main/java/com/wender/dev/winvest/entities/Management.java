0757ca635fc4637acbbee5ea835b5be4d2930c97
Wender Rocha <wender_dev@hotmail.com>
2021-10-28 14:35:40 -0400

    Entity Management

---
+++ src/main/java/com/wender/dev/winvest/entities/Management.java
@@ -0,0 +1,13 @@
+package com.wender.dev.winvest.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Management implements Serializable {
+
+    private Long id;
+    private BigDecimal target;
+
+    private Wallet wallet;
+    
+}
