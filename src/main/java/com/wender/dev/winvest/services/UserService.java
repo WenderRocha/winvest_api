f034434954a15d8ca6b4299ec38f928cde03c987
Wender Rocha <wender_dev@hotmail.com>
2021-10-28 11:01:56 -0400

    Service layer, component registration

---
+++ src/main/java/com/wender/dev/winvest/services/UserService.java
@@ -0,0 +1,24 @@
+package com.wender.dev.winvest.services;
+
+import com.wender.dev.winvest.entities.User;
+import com.wender.dev.winvest.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
