c201c92d0967ac25acc417821a7d58a9c99a19fa
Wender Rocha <wender_dev@hotmail.com>
2021-10-28 10:36:06 -0400

    H2 database, test profile, JPA

--- src/main/java/com/wender/dev/winvest/entities/User.java
+++ src/main/java/com/wender/dev/winvest/entities/User.java
@@ -1,7 +1,9 @@
 package com.wender.dev.winvest.entities;
 
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @NoArgsConstructor
@@ -9,12 +11,24 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
+    private String phone;
+
+    @EqualsAndHashCode.Include
+    @CPF
     private String cpf;
+
     private String password;
 }
