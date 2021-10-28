dd22f1a79295e0ae07b31604cce2f45cfca466f9
Wender Rocha <wender_dev@hotmail.com>
2021-10-28 10:18:44 -0400

    Project created

---
+++ src/main/java/com/wender/dev/winvest/WinvestApplication.java
@@ -0,0 +1,13 @@
+package com.wender.dev.winvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wender/dev/winvest/WinvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wender.dev.winvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
