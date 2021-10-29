fc4318377f27f6d492e5d5d24a1425cfcc56e07e
Wender Rocha <wender_dev@hotmail.com>
2021-10-29 17:59:26 -0400

    create ObjectNotFoundException

--- src/main/java/com/wender/dev/winvest/services/UserService.java
+++ src/main/java/com/wender/dev/winvest/services/UserService.java
@@ -2,6 +2,7 @@ package com.wender.dev.winvest.services;
 
 import com.wender.dev.winvest.entities.User;
 import com.wender.dev.winvest.repositories.UserRepository;
+import com.wender.dev.winvest.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,9 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found! id: "
+                + id
+                + ", Type: "
+                + User.class.getName()));
     }
 }
