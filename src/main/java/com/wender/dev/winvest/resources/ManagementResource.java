faea9e608f56a6a541bb35964bb92d0b649bcb2d
Wender Rocha <wender_dev@hotmail.com>
2021-11-07 21:58:53 -0300

    add crossorigin

--- src/main/java/com/wender/dev/winvest/resources/ManagementResource.java
+++ src/main/java/com/wender/dev/winvest/resources/ManagementResource.java
@@ -13,6 +13,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/managements")
 public class ManagementResource {
